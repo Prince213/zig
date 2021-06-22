2710c9d6932b568f24eb129b0bd4d53a797ae902
Jakub Konka <kubkon@jakubkonka.com>
2021-06-22 12:47:56 +0200

    Classify .m as ObjC, compile using clang and link with zld

--- src/Compilation.zig
+++ src/Compilation.zig
@@ -2843,7 +2843,7 @@ pub fn addCCArgs(
     try argv.appendSlice(&[_][]const u8{ "-target", llvm_triple });
 
     switch (ext) {
-        .c, .cpp, .h => {
+        .c, .cpp, .m, .h => {
             try argv.appendSlice(&[_][]const u8{
                 "-nostdinc",
                 "-fno-spell-checking",
@@ -3134,6 +3134,7 @@ pub const FileExt = enum {
     c,
     cpp,
     h,
+    m,
     ll,
     bc,
     assembly,
@@ -3145,7 +3146,7 @@ pub const FileExt = enum {
 
     pub fn clangSupportsDepFile(ext: FileExt) bool {
         return switch (ext) {
-            .c, .cpp, .h => true,
+            .c, .cpp, .h, .m => true,
 
             .ll,
             .bc,
@@ -3177,6 +3178,10 @@ pub fn hasCppExt(filename: []const u8) bool {
         mem.endsWith(u8, filename, ".cc") or
         mem.endsWith(u8, filename, ".cpp") or
         mem.endsWith(u8, filename, ".cxx");
+}
+
+pub fn hasObjCExt(filename: []const u8) bool {
+    return mem.endsWith(u8, filename, ".m");
 }
 
 pub fn hasAsmExt(filename: []const u8) bool {
@@ -3215,6 +3220,8 @@ pub fn classifyFileExt(filename: []const u8) FileExt {
         return .c;
     } else if (hasCppExt(filename)) {
         return .cpp;
+    } else if (hasObjCExt(filename)) {
+        return .m;
     } else if (mem.endsWith(u8, filename, ".ll")) {
         return .ll;
     } else if (mem.endsWith(u8, filename, ".bc")) {
@@ -3238,6 +3245,7 @@ pub fn classifyFileExt(filename: []const u8) FileExt {
 
 test "classifyFileExt" {
     try std.testing.expectEqual(FileExt.cpp, classifyFileExt("foo.cc"));
+    try std.testing.expectEqual(FileExt.m, classifyFileExt("foo.m"));
     try std.testing.expectEqual(FileExt.unknown, classifyFileExt("foo.nim"));
     try std.testing.expectEqual(FileExt.shared_library, classifyFileExt("foo.so"));
     try std.testing.expectEqual(FileExt.shared_library, classifyFileExt("foo.so.1"));
