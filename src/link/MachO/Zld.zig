4ff635fd00900e82a26f3e590beca69971a1f5e6
Jakub Konka <kubkon@jakubkonka.com>
2021-06-24 18:43:38 +0200

    zld: exclude libs part of the umbrella
    when parsing the umbrella lib

--- src/link/MachO/Zld.zig
+++ src/link/MachO/Zld.zig
@@ -278,7 +278,6 @@ fn parseInputFiles(self: *Zld, files: []const []const u8) !void {
             self.arch.?,
             full_path,
             self.syslibroot,
-            true,
         )) |dylibs| {
             defer self.allocator.free(dylibs);
             try self.dylibs.appendSlice(self.allocator, dylibs);
@@ -296,7 +295,6 @@ fn parseLibs(self: *Zld, libs: []const []const u8) !void {
             self.arch.?,
             lib,
             self.syslibroot,
-            true,
         )) |dylibs| {
             defer self.allocator.free(dylibs);
             try self.dylibs.appendSlice(self.allocator, dylibs);
@@ -318,7 +316,6 @@ fn parseLibSystem(self: *Zld, libc_stub_path: []const u8) !void {
         self.arch.?,
         libc_stub_path,
         self.syslibroot,
-        false,
     )) orelse return error.FailedToParseLibSystem;
     defer self.allocator.free(dylibs);
 
